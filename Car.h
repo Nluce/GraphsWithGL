7600c333e887927da08183d4d4159fc47c4c9102
Nluce <noluce@me.com>
2015-03-24 13:14:13 -0700

    3/24

--- Car.h
+++ Car.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "GraphNode.h"
 #include "Graph.h"
-
+#include "NodeCity.h"
 #include <glm\glm.hpp>
 
 using namespace glm;
@@ -12,6 +12,8 @@ public:
 	const Graph& graph;
 	const vector <ivec2> checkpoints;
 	int currentCheckpoint = 0;
+	glm::vec2 position;
+	glm::vec2 direction;
 	Car(ivec2 spawnPoint, const Graph& graph, const vector <ivec2> checkpoints) :
 		graph(graph),
 		checkpoints(checkpoints)
@@ -21,6 +23,11 @@ public:
 		GraphNode* end = graph.FindClosestNode(goal);
 		//path = graph.FindPath(start, end);
 	}
+	void SpawnCars()
+	{
+		
+	}
+
 	~Car();
 	vector <GraphNode> path;
 
